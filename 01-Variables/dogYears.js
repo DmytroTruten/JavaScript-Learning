908d8aba15dcea0686437bdd364a17aa1de410de
DmitryTruten <dmitriy2503a@gmail.com>
2022-09-11 18:57:23 +0300

    practice with variables

---
+++ 01-Variables/dogYears.js
@@ -0,0 +1,16 @@
+// create variable with name
+let myName = 'Dmitriy';
+
+// create variable with age
+const myAge = 19;
+
+// calculating dog years
+let earlyYears = 2 * 10.5;
+let laterYears = (myAge - 2) * 4;
+let myAgeInDogYears = earlyYears + laterYears;
+
+//assigning my name to lower case
+myName = myName.toLowerCase();
+
+//console.log the result
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
