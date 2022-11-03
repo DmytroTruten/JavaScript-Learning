f36411919be0f7c3c429ba9548f52521d7cbc9ec
DmitriyTruten <dmitriy2503a@gmail.com>
2022-11-03 14:08:24 +0200

    first half of learning jQuery setup

---
+++ jQuery Learn/.ready()/main.js
@@ -0,0 +1,8 @@
+/* .ready() method waits until the HTML page’s DOM is ready to manipulate. 
+You should wrap all JavaScript behavior inside of the .ready() method. 
+This will make sure the web page is rendered in the browser before 
+any jQuery code executes */
+
+$(document).ready(() => {
+
+})
