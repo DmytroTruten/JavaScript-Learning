a990610945a7dbd877d7c2775c3bfa382092648b
DmitriyTruten <dmitriy2503a@gmail.com>
2022-11-04 17:40:34 +0200

    learned basic mouse events

---
+++ jQuery Learn/Mouse Events/mouseenter/main.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+  $('.login-button').on('click', () => {
+    $('.login-form').show();
+  });
+  $(".menu-button").on('mouseenter', () => {
+    $(".nav-menu").show();
+  })
+
+}); 
+
