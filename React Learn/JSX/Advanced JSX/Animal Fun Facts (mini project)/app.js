c1043b9b2d19b0380a6469218e58e161ca65078f
DmitriyTruten <dmitriy2503a@gmail.com>
2022-11-08 17:39:01 +0200

    make mini project

--- React Learn/JSX/Advanced JSX/Animal Fun Facts (mini project)/app.js
+++ React Learn/JSX/Advanced JSX/Animal Fun Facts (mini project)/app.js
@@ -1,6 +1,6 @@
 import { animals } from "./animals";
 import React from "react";
-import { ReactDOM } from "react-dom";
+import ReactDOM from "react-dom";
 
 const title = "";
 const background = (
@@ -8,10 +8,33 @@ const background = (
     <img className="background" src="./images/ocean.jpg" alt="ocean" />
   </div>
 );
+const images = [];
+for (const animal in animals) {
+  images.push(
+    <img
+      src={animals[animal].image}
+      alt={animal}
+      className="animal"
+      ariaLabel={animal}
+      role="button"
+      onClick={displayFact}
+    />
+  );
+}
+
+const displayFact = (e) => {
+  const selectedAnimal = e.target.alt;
+  const animalInfo = animals[selectedAnimal];
+  const optionIndex = Math.floor(Math.random() * animals.facts.length);
+  const funFact = animalInfo.facts[optionIndex];
+  document.getElementById('fact').innerHTML = funFact;
+};
 const animalFacts = (
   <div>
     <h1>{title === "" ? "Click an animal for a fun fact" : title}</h1>
     {background}
+    <p id="fact"></p>
+    <div className="animals">{images}</div>
   </div>
 );
 
