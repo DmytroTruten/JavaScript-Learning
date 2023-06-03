8e0eb0bfcf218a4c2348aad469ecd203bcaeec1a
Dmytro Truten <dmitriy2503a@gmail.com>
2023-06-03 18:56:02 +0300

    add lesson about functions

---
+++ TypeScript Learn/02-Functions/Default Parameters/index.ts
@@ -0,0 +1,9 @@
+function proclaim1(status = "not ready...", repeat = 1) {
+  for (let i = 0; i < repeat; i += 1) {
+    console.log(`I'm ${status}`);
+  }
+}
+
+proclaim1();
+proclaim1('ready?');
+proclaim1('ready!', 3);
---
+++ TypeScript Learn/02-Functions/Documenting Functions/index.ts
@@ -0,0 +1,25 @@
+/**
+ * Prints the ingredients in salad to the console
+ * @param fruit1 - first fruit
+ * @param fruit2 - second fruit
+ * @returns void(Nothing)
+ */
+
+function makeFruitSalad1(fruit1: string, fruit2: string): void {
+  let salad = fruit1 + fruit2 + fruit2 + fruit1 + fruit2+ fruit1 + fruit1;
+  console.log(salad);
+}
+
+/**
+ * Prints the status to the console
+ * @param status - default parameter, part of a final status
+ * @param repeat - the number of times the function will be called
+ * @returns - void(Nothing)
+ */
+
+
+function proclaim(status = 'not ready...', repeat = 1) {
+  for (let i = 0; i < repeat; i += 1) {
+    console.log(`I'm ${status}`);
+  }
+}
