337cfb8fe2d83d6fffc3f7dd49ba5bdf94df9a04
Dmytro Truten <dmitriy2503a@gmail.com>
2023-06-04 13:31:17 +0300

    add lesson with arrays

---
+++ TypeScript Learn/03-Arrays/Multi-dimensional Arrays/index.ts
@@ -0,0 +1,15 @@
+// Arrays:
+let bestNumbers1: number[] = [7, 77, 4];
+let bestLunches1: string[] = ["chicken soup", "non-chicken soup"];
+let bestBreakfasts1: string[] = [
+  "scrambled eggs",
+  "oatmeal",
+  "tamago kake gohan",
+  "any kind of soup",
+];
+let bestBooleans1: boolean[] = [true, false];
+
+// Multidimensional Arrays:
+let bestMealPlan: string[][] = [bestLunches1, bestBreakfasts1, ['baked potato', 'mashed potato']];
+let bestBooleansTwice: boolean[][] = [bestBooleans1, bestBooleans1];
+let numbersMulti: number[][][] = [ [[1],[2,3]], [[7],bestNumbers1] ];
